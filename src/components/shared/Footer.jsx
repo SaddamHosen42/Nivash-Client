7b34e45524c78b8c63aed112691afc6dd9dfa10c
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-07-16 21:58:48 +0600

    update:footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
             <ul className="space-y-2">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="text-gray-300 hover:text-blue-400 transition-colors"
                 >
                   Home
@@ -48,7 +48,7 @@ const Footer = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/apartment"
                   className="text-gray-300 hover:text-blue-400 transition-colors"
                 >
                   Apartments
@@ -154,7 +154,7 @@ const Footer = () => {
                 >
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
-                <span className="text-gray-300">+880 1234567890</span>
+                <span className="text-gray-300">+880 1627482575</span>
               </div>
               <div className="flex items-center space-x-3">
                 <svg
@@ -165,7 +165,7 @@ const Footer = () => {
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
-                <span className="text-gray-300">info@buildinghub.com</span>
+                <span className="text-gray-300">saddamhosen1433@gmail.com</span>
               </div>
             </div>
           </div>
@@ -175,7 +175,7 @@ const Footer = () => {
         <div className="mt-12 pt-8 border-t border-gray-700">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-sm">
-              © 2025 BuildingHub. All rights reserved.
+              © 2025 Nivash Team. All rights reserved.
             </p>
             <div className="flex space-x-6 mt-4 md:mt-0">
               <a
